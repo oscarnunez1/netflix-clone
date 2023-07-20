4ff83ea870caf08eecd2a64004cbfbd1afc6fd9c
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-07-20 16:16:50 -0400

    style input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,9 @@
 const Input = () => {
   return (
-    <input />
+    <input
+      className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"
+      placeholder=" "
+    />
   )
 }
 
