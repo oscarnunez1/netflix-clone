48d583f78eff66513a19122291fc819e2a403433
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-07-20 16:04:01 -0400

    create inout component, import Input to auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,5 @@
+import Input from "@/components/input"
+
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -7,7 +9,12 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              Sign In 
+            </h2>
+            <div className="flex flex-col gap-4">
+              <Input />
+            </div>
           </div>
         </div>
       </div>
